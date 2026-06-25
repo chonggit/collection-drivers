f43b2c40c591d9ea8c1c8a2c65143830f2b0575c
cihong.liu <cihong.liu@foxmail.com>
2026-06-25 14:26:37 +0800

    feat: add OpcUaMachine and YAML config example

---
+++ opcua-driver/OpcUaMachine.cs
@@ -0,0 +1,10 @@
+using l99.driver.@base;
+
+namespace opcua.driver;
+
+public class OpcUaMachine : Machine
+{
+    public OpcUaMachine(Machines machines, object configuration) : base(machines, configuration)
+    {
+    }
+}

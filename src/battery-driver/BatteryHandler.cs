6c10e04607dcca29135f9ec3099087546d3cfa6e
cihong.liu <cihong.liu@foxmail.com>
2026-06-26 14:21:50 +0800

    refactor: 移除 DataPublisher 类及其相关测试，简化 BatteryHandler 实现

--- src/battery-driver/BatteryHandler.cs
+++ src/battery-driver/BatteryHandler.cs
@@ -1,25 +1,10 @@
-using battery.driver.channels;
-using battery.driver.models;
 using l99.driver.@base;
 
 namespace battery.driver;
 
-public class BatteryHandler : Handler, IDisposable
+public class BatteryHandler : Handler
 {
-    private readonly DataPublisher _publisher = new();
-
     public BatteryHandler(Machine machine) : base(machine)
     {
     }
-
-    public void Publish(ChannelRealData data) => _publisher.Publish(data);
-    public void Publish(AlarmData data) => _publisher.Publish(data);
-    public void Publish(ResultData data) => _publisher.Publish(data);
-    public void Publish(StatusData data) => _publisher.Publish(data);
-    public void Publish(WarningData data) => _publisher.Publish(data);
-    public void Publish(AckData data) => _publisher.Publish(data);
-
-    public DataPublisher Publisher => _publisher;
-
-    public void Dispose() => _publisher.Dispose();
 }
